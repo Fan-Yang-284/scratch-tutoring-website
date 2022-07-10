848c57be17dac1fe061a78144c8e5a28bfe6245b
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-07-10 14:15:28 -0400

    Image Update for Scratch Course

--- data/courses.ts
+++ data/courses.ts
@@ -5,7 +5,7 @@ const courses = [
         description: "Scratch is a visual programming language developed by MIT. Learn to code with Scratch.",
         startDate: new Date(1658754000000),
         endDate: new Date(1659106800000),
-        backgroundImage: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        backgroundImage: "/scratch2022Banner.jpg",
         courseURL: "/scratch2022",
         price: 100,
     },
