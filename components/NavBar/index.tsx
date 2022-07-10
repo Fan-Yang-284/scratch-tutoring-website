81d1a8eee27d3592ba0c47c003614d2280c1ab20
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-07-10 14:24:56 -0400

    Register Button

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
 						<Link href="/register">
 							<a>
 								<button type="button" className="text-white bg-blue-700 hover:bg-blue-800 transition-colors focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 lg:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-									Get started
+									Register Today
 								</button>
 							</a>
 						</Link>
