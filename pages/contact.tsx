37e97fa1baa69e631b8291f7e8fde0d5a91c71ac
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-07-09 20:49:55 -0400

    bruh contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,21 @@
+const PersonCard = ({ name, profileSrc, email }: { name: string, profileSrc: string, email: string})=>{
+    return(
+        <div className="p-4 border-2">
+            <img src={profileSrc} className="h-48 md:h-56 w-48 md:w-56 rounded-lg" alt="" />
+            <h1 className="font-bold text-6xl mt-4">
+                {name}
+            </h1>
+            <a href={"mailto:"+email} className="text-blue-400 hover:text-blue-600 visited:text-purple-400">
+                {email}
+            </a>
+        </div>
+    )
+}
 const contact = () => {
     return (
-        <div>
-            Contact
+        <div className="flex flex-col md:flex-row mx-4 pt-24 justify-around gap-y-4 gap-x-6">
+            <PersonCard name="Edwin Zheng" profileSrc="/Edwin_Avatar.png" email="jessicawu414@gmail.com"/>
+            <PersonCard name="Fan Yang" profileSrc="/Fan_Avatar.png" email="fan.yang284@gmail.com"/>
         </div>
     );
 }
