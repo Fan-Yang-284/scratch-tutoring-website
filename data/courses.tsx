c8fd9f67c66cc889bb3e151c1018b7fe20a8a9d9
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-07-14 09:58:11 -0400

    Scratch Trial Class 2022

--- data/courses.tsx
+++ data/courses.tsx
@@ -1,6 +1,24 @@
 import CourseType from "../types/Course";
 const courses = [
     {
+        title: "Scratch Trial 2022",
+        courseID: "scratchTrial2022",
+        description: "Trial Class for Scratch 2022 Class. This class will teach students how to make a coin collection game.",
+        startDate: new Date(1658062800000),
+        endDate: new Date(1658066400000),
+        backgroundImage: "/scratchTrial2022Banner.png",
+        price: 0,
+        teachers: [
+            {
+                name: "Edwin Zheng",
+                avatarURL: "/Edwin_Avatar.png"
+            },
+        ],
+        nextSteps: [
+            <>Join with this <a href="https://us04web.zoom.us/j/6408315738?pwd=QrLIcFcIaCqf4UARlYMvcDIRr6TmfM.1" className="text-blue-500 underline">Zoom link</a></>
+        ]
+    },
+    {
         title: "Scratch 2022",
         courseID: "scratch2022",
         description: "Scratch is a visual programming language developed by MIT. Learn to code with Scratch.",
