f3aea068cee2c3cd20c9f127b353597f71644ee6
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-07-14 09:59:07 -0400

    small changes
    minimum height for coursecard to align bottoms
    register button now collapses navbar in mobile views
    Register success page throws errors properly + price aware.

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -68,7 +68,7 @@ const Navbar = () => {
 					<div className="flex items-center">
 						<Link href="/register">
 							<a>
-								<button type="button" className="text-white bg-blue-700 hover:bg-blue-800 transition-colors focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 lg:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+								<button type="button" onClick={()=>{toggleOpen(false)}} className="text-white bg-blue-700 hover:bg-blue-800 transition-colors focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 lg:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
 									Register Today
 								</button>
 							</a>
